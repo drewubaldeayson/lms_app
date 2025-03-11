c090db270681c620934bc49bc490af82d03f0806
Andrew Ayson <drewubaldeayson@gmail.com>
2025-03-12 00:36:09 +0800

    update changes

--- frontend/src/pages/ContentPage.js
+++ frontend/src/pages/ContentPage.js
@@ -12,8 +12,12 @@ import axios from 'axios';
 import EditIcon from '@mui/icons-material/Edit';
 import SaveIcon from '@mui/icons-material/Save';
 import CancelIcon from '@mui/icons-material/Cancel';
+import InfoBox from '../components/Content/InfoBox';
+import CautionBox from '../components/Content/CautionBox';
+import WarningBox from '../components/Content/WarningBox';
+import NoteBox from '../components/Content/NoteBox';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://170.64.202.114:5000';
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
 
 const ContentPage = ({ setHeadings }) => {
   const { '*': path } = useParams();
@@ -206,7 +210,7 @@ const ContentPage = ({ setHeadings }) => {
       )} */}
 
 
-      {/* <Box sx={{ position: 'absolute', top: 16, right: 16, zIndex: 1 }}>
+      <Box sx={{ position: 'absolute', top: 16, right: 16, zIndex: 1 }}>
           {!isEditing ? (
             <IconButton onClick={handleEdit} color="primary">
               <EditIcon />
@@ -221,7 +225,7 @@ const ContentPage = ({ setHeadings }) => {
               </IconButton>
             </Box>
           )}
-      </Box> */}
+      </Box>
 
       {/* Markdown content */}
       <Paper sx={{ p: 3 }}>
@@ -240,6 +244,95 @@ const ContentPage = ({ setHeadings }) => {
           />
         ) :  <ReactMarkdown
           components={{
+            blockquote: ({ node, children, ...props }) => {
+              console.log("BLOCKQUOTE: ", children);
+              // Helper function to extract text from children recursively
+              const extractText = (child) => {
+                if (typeof child === 'string') {
+                  return child;
+                } else if (React.isValidElement(child)) {
+                  return React.Children.toArray(child.props.children).map(extractText).join('');
+                }
+                return '';
+              };
+      
+              const textContent = children.map(extractText).join('').trim();
+
+              const markers = {
+                '[!INFO]': InfoBox,
+                '[!WARNING]': WarningBox,
+                '[!CAUTION]': CautionBox,
+                '[!NOTE]': NoteBox,
+              };
+
+      
+              for (const [marker, Component] of Object.entries(markers)) {
+                if (textContent.startsWith(marker)) {
+                  // Pass the original children to preserve markdown syntax
+                  const contentWithoutMarker = textContent.slice(marker.length).trim();
+                  return (
+                    <Component>
+                      <ReactMarkdown
+                      components={{ 
+                        code({ node, inline, className, children, ...props }) {
+                          const match = /language-(\w+)/.exec(className || '');
+                          return !inline && match ? (
+                            <SyntaxHighlighter
+                              style={tomorrow}
+                              language={match[1]}
+                              PreTag="div"
+                              {...props}
+                            >
+                              {String(children).replace(/\n$/, '')}
+                            </SyntaxHighlighter>
+                          ) : (
+                            <code className={className} {...props}>
+                              {children}
+                            </code>
+                          );
+                        },
+                        img: ImageComponent,
+                        h1: ({ node, ...props }) => (
+                          <h1
+                            id={props.children.toString().toLowerCase().replace(/[^\w]+/g, '-')}
+                            style={{
+                              scrollMarginTop: '80px', // Add scroll margin for smooth scrolling
+                              transition: 'background-color 0.3s ease' // Add transition for highlight effect
+                            }}
+                            {...props}
+                          />
+                        ),
+                        h2: ({ node, ...props }) => (
+                          <h2
+                            id={props.children.toString().toLowerCase().replace(/[^\w]+/g, '-')}
+                            style={{
+                              scrollMarginTop: '80px',
+                              transition: 'background-color 0.3s ease'
+                            }}
+                            {...props}
+                          />
+                        ),
+                        h3: ({ node, ...props }) => (
+                          <h3
+                            id={props.children.toString().toLowerCase().replace(/[^\w]+/g, '-')}
+                            style={{
+                              scrollMarginTop: '80px',
+                              transition: 'background-color 0.3s ease'
+                            }}
+                            {...props}
+                          />
+                        ),
+                      
+                      }}>{contentWithoutMarker }</ReactMarkdown>
+                    </Component>
+                  );
+                }
+              }
+      
+              return <blockquote>{children}</blockquote>;
+            },
+            
+            
             code({ node, inline, className, children, ...props }) {
               const match = /language-(\w+)/.exec(className || '');
               return !inline && match ? (
