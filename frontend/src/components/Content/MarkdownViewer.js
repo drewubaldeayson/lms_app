4daeeeeebd96255694868271dd173925a1b2d37b
Andrew Ayson <drewubaldeayson@gmail.com>
2025-03-11 15:58:54 +0800

    update changes

--- frontend/src/components/Content/MarkdownViewer.js
+++ frontend/src/components/Content/MarkdownViewer.js
@@ -30,7 +30,7 @@ const MarkdownViewer = ({ content }) => {
             }
         
             return <blockquote>{children}</blockquote>;
-        }
+        },
         
           code({ node, inline, className, children, ...props }) {
             const match = /language-(\w+)/.exec(className || '');
